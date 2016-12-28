358cf17ecb51a6838c261a0bd398edb3c2c46782
Jae S Choi <choijaeseok@gmail.com>
2016-12-28 11:24:20 -0400

    $(comment)

--- R/hivemod_attach.R
+++ R/hivemod_attach.R
@@ -1,5 +1,6 @@
 
 hivemod_attach = function( method, pointer ) {
+  #\\ eneric method to attach data pointer from bigmemory or ff 
    return( 
       switch( method, 
         bigmemory.ram=attach.big.matrix(pointer), 
