55fe3d696b70ee4e269ea04f16500e2782517280
Jae S Choi <choijaeseok@gmail.com>
2016-12-28 11:17:39 -0400

    misc

--- R/hivemod__splancs.R
+++ R/hivemod__splancs.R
@@ -1,8 +1,6 @@
 
 hivemod__splancs = function( p, x, pa, phi=NULL, nu=NULL ) {
   #\\ this is the core engine of hivemod .. localised space (no-time) modelling interpolation 
-  #\\ note: time is not being modelled and treated independently 
-  #\\      .. you had better have enough data in each time slice
 
   return( list( predictions=pa, hivemod_stats=hivemod_stats ) )  
 }
