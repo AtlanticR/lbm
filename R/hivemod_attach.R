74a0d335eea8ad4aa22d06926c3359521e380304
Jae S Choi <choijaeseok@gmail.com>
2016-12-28 11:35:26 -0400

    "misc"

--- R/hivemod_attach.R
+++ R/hivemod_attach.R
@@ -1,11 +1,11 @@
 
 hivemod_attach = function( method, pointer ) {
   #\\ generic method to attach data pointer from bigmemory or ff storage pointers
-   return( 
-      switch( method, 
-        bigmemory.ram=attach.big.matrix(pointer), 
-        bigmemory.filebacked=attach.big.matrix(pointer), 
-        ff=pointer )
-    )
+  return( 
+    switch( method, 
+      bigmemory.ram=attach.big.matrix(pointer), 
+      bigmemory.filebacked=attach.big.matrix(pointer), 
+      ff=pointer )
+  )
 }
 
